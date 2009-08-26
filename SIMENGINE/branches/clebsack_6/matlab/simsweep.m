59cca9846a7da193e335b755befdc9c81d7c217f
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-08-26 14:41:24 +0000

    changed name of simview to simsweep to better describe its limited purpose

--- SIMENGINE/branches/clebsack_6/matlab/simsweep.m
+++ SIMENGINE/branches/clebsack_6/matlab/simsweep.m
@@ -1,35 +1,35 @@
-function varargout = simview(varargin)
-% SIMVIEW M-file for simview.fig
-%      SIMVIEW, by itself, creates a new SIMVIEW or raises the existing
+function varargout = simsweep(varargin)
+% SIMSWEEP M-file for simsweep.fig
+%      SIMSWEEP, by itself, creates a new SIMSWEEP or raises the existing
 %      singleton*.
 %
-%      H = SIMVIEW returns the handle to a new SIMVIEW or the handle to
+%      H = SIMSWEEP returns the handle to a new SIMSWEEP or the handle to
 %      the existing singleton*.
 %
-%      SIMVIEW('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in SIMVIEW.M with the given input arguments.
+%      SIMSWEEP('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SIMSWEEP.M with the given input arguments.
 %
-%      SIMVIEW('Property','Value',...) creates a new SIMVIEW or raises the
+%      SIMSWEEP('Property','Value',...) creates a new SIMSWEEP or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before simview_OpeningFcn gets called.  An
+%      applied to the GUI before simsweep_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to simview_OpeningFcn via varargin.
+%      stop.  All inputs are passed to simsweep_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help simview
+% Edit the above text to modify the response to help simsweep
 
-% Last Modified by GUIDE v2.5 25-Aug-2009 18:18:52
+% Last Modified by GUIDE v2.5 26-Aug-2009 10:26:03
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @simview_OpeningFcn, ...
-                   'gui_OutputFcn',  @simview_OutputFcn, ...
+                   'gui_OpeningFcn', @simsweep_OpeningFcn, ...
+                   'gui_OutputFcn',  @simsweep_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before simview is made visible.
-function simview_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before simsweep is made visible.
+function simsweep_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to simview (see VARARGIN)
+% varargin   command line arguments to simsweep (see VARARGIN)
 
-% Choose default command line output for simview
+% Choose default command line output for simsweep
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes simview wait for user response (see UIRESUME)
+% UIWAIT makes simsweep wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = simview_OutputFcn(hObject, eventdata, handles) 
+function varargout = simsweep_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
