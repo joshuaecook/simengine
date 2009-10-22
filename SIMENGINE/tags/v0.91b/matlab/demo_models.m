ba92698f1657cccf079e43495d7037c4a02e32fd
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-10-22 18:26:24 +0000

    Fixed demo_models.m to include correct paths

--- SIMENGINE/tags/v0.91b/matlab/demo_models.m
+++ SIMENGINE/tags/v0.91b/matlab/demo_models.m
@@ -7,7 +7,7 @@ demo = struct();
 
 % start with a brk model
 demo(i).title = 'BRK - Spike Train';
-demo(i).file = [simexamplepath '/brk.dsl'];
+demo(i).file = [simexamplepath '/BRK/brk.dsl'];
 demo(i).starttime = 0;
 demo(i).stoptime = 100;
 demo(i).inputs.Iext = [0 40];
@@ -17,8 +17,8 @@ demo(i).target = 'parallel-cpu';
 
 i = i + 1;
 % next, go to stg
-demo(i).title = 'STG - Adjust Sodium';
-demo(i).file = [simexamplepath '/stg_burster.dsl'];
+demo(i).title = 'STG/PD - Adjust Sodium';
+demo(i).file = [simexamplepath '/PD/pd.dsl'];
 demo(i).starttime = 0;
 demo(i).stoptime = 500;
 demo(i).inputs.gNa = [0 200];
@@ -27,21 +27,9 @@ demo(i).precision = 'single';
 demo(i).target = 'parallel-cpu';
 
 i = i + 1;
-% next, try pBc
-demo(i).title = 'pBc - preBotzinger Complex Persistant Na Sweep';
-demo(i).file = [simexamplepath '/pbc.dsl'];
-demo(i).starttime = 0;
-demo(i).stoptime = 2;
-demo(i).inputs.Iext = 8;
-demo(i).inputs.gNaP = [2.5 3];
-demo(i).steps = 100;
-demo(i).precision = 'single';
-demo(i).target = 'parallel-cpu';
-
-i = i + 1;
 % next, try FN
 demo(i).title = 'FN - Sweep Current';
-demo(i).file = [simexamplepath '/fn.dsl'];
+demo(i).file = [simexamplepath '/FN/fn.dsl'];
 demo(i).starttime = 0;
 demo(i).stoptime = 100;
 demo(i).inputs.I = [0 4];
