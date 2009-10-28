ce5c68b4913941cb4705d054f74760001585f218
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-10-28 19:44:07 +0000

    Fixed typo in simex.m

--- SIMENGINE/branches/RandyChris_9/interfaces/matlab/simex.m
+++ SIMENGINE/branches/RandyChris_9/interfaces/matlab/simex.m
@@ -525,14 +525,14 @@ end
 end % end if recompile
 
 % TODO what is the path of the resultant DLL?
-switch computer
+%switch computer
 %  case {'MACI', 'MACI64','i386-apple-darwin9.8.0','i386-apple-darwin8.9.1'}
 %   dllPath = fullfile(pwd, 'libsimengine.dylib');
 % otherwise
 %   dllPath = fullfile(pwd, 'libsimengine.so');
 [igpath modelname igext] = fileparts(modelFile);
 dllPath = fullfile(pwd, [modelname '.sim']);
-end  
+%end  
 
 % TODO check the shape of the user inputs and start states, other
 % parameters, and recompile the model if necessary.
