0f27af575f7c69cde57adcd0894835c621ef2756
clebsack <clebsack@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-11-06 22:31:44 +0000

    Updated simlib to properly handle both 32bit and 64bit mach-o and elf objects.

--- SIMENGINE/branches/RandyChris_9/codegen/simlib/simlib.c
+++ SIMENGINE/branches/RandyChris_9/codegen/simlib/simlib.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<dlfcn.h>
-#include <assert.h>
-#include <zlib.h>
+#include<assert.h>
+#include<zlib.h>
 
 #ifdef DARWIN
 #include<mach-o/loader.h>
@@ -36,17 +36,17 @@ void close_binary(){
 }
 
 inline int round_to_word(int offset){
-  if(sizeof(void*) == 4)
-    return (offset & 0x3) ? offset - (offset & 0x3) + 0x4 : offset;
-  else
-    return (offset & 0x7) ? offset - (offset & 0x7) + 0x8 : offset;
+  //if(4 == sizeof(void*))
+  return (offset & 0x3) ? offset - (offset & 0x3) + 0x4 : offset;
+  //else
+  //return (offset & 0x7) ? offset - (offset & 0x7) + 0x8 : offset;
 }
 
 inline int pad_len(int offset){
-  if(sizeof(void*) == 4)
-    return (0x4 - (offset & 0x3)) & 0x3;
-  else
-    return (0x8 - (offset & 0x7)) & 0x7;
+  //if(sizeof(void*) == 4)
+  return (0x4 - (offset & 0x3)) & 0x3;
+  //else
+  //return (0x8 - (offset & 0x7)) & 0x7;
 }
 
 void fix_fname(char *fname){
@@ -224,7 +224,7 @@ unsigned long extract_binary_file(const char *fname, unsigned char **data){
 }
 
 #ifdef DARWIN
-char *write_mach_o(char *fname, int dsize, char *data){
+char *write_mach_o32(char *fname, int dsize, char *data){
   // Mach-O structures
   struct mach_header mh;
   struct segment_command segc;
@@ -361,13 +361,145 @@ char *write_mach_o(char *fname, int dsize, char *data){
   return ofname; // Must be freed externally
 }
 
+char *write_mach_o64(char *fname, int dsize, char *data){
+  // Mach-O structures
+  struct mach_header_64 mh;
+  struct segment_command_64 segc;
+  struct section_64 sec;
+  struct symtab_command symc;
+  struct dysymtab_command dsymc;
+  struct nlist_64 nl[NUM_SYMBOLS];
+  // Other vars
+  FILE *objfile;
+  char *fname_fixed;
+  char *ofname;
+  char *sym_beg;
+  char *sym_end;
+  int len = strlen(fname);
+
+  // Mach-o Header
+  mh.magic = MH_MAGIC_64;
+  mh.cputype = CPU_TYPE_X86_64;
+  mh.cpusubtype = CPU_SUBTYPE_X86_64_ALL;
+  mh.filetype = MH_OBJECT;
+  mh.ncmds = 3;
+  mh.sizeofcmds = 256;
+  mh.flags = 0;
+
+  // Data Segment/Section 64bit
+  // Segment
+  segc.cmd = LC_SEGMENT_64;
+  segc.cmdsize = sizeof(segc) + sizeof(sec);
+  segc.segname[0] = 0; // Intermediate objects have only one segment with no name
+  segc.vmaddr = 0;
+  segc.vmsize = round_to_word(dsize); // GENERATED
+  segc.fileoff = 288; // Beginning of contents after header and load commands
+  segc.filesize = segc.vmsize; // GENERATED
+  segc.maxprot = 1; // Read-only
+  segc.initprot = 1; // Read-only
+  segc.nsects = 1;
+  segc.flags = 0;
+  // Section
+  strcpy(sec.sectname, "__const");
+  strcpy(sec.segname, "__DATA");
+  sec.addr = 0;
+  sec.size = round_to_word(dsize); //GENERATED
+  sec.offset = 288;
+  sec.align = 2;
+  sec.reloff = segc.vmsize + sec.offset; //GENERATED
+  sec.nreloc = 0;
+  sec.flags = 0;
+  sec.reserved1 = 0;
+  sec.reserved2 = 0;
+
+  // Symbol Table
+  symc.cmd = LC_SYMTAB;
+  symc.cmdsize = sizeof(symc);
+  symc.symoff = sec.reloff; // GENERATED
+  symc.nsyms = NUM_SYMBOLS;
+  symc.stroff = symc.symoff + symc.nsyms * sizeof(struct nlist_64); // GENERATED
+  symc.strsize = NUM_SYMBOLS*(len + sfx_len + 1); // GENERATED
+
+  // Dynamic Symbol Table
+  dsymc.cmd = LC_DYSYMTAB;
+  dsymc.cmdsize = sizeof(dsymc);
+  dsymc.ilocalsym = 0;
+  dsymc.nlocalsym = 0;
+  dsymc.iextdefsym = 0;
+  dsymc.nextdefsym = NUM_SYMBOLS;
+  dsymc.iundefsym = NUM_SYMBOLS;
+  dsymc.nundefsym = 0;
+  dsymc.tocoff = 0;
+  dsymc.ntoc = 0;
+  dsymc.modtaboff = 0;
+  dsymc.nmodtab = 0;
+  dsymc.extrefsymoff = 0;
+  dsymc.nextrefsyms = 0;
+  dsymc.indirectsymoff = 0;
+  dsymc.nindirectsyms = 0;
+  dsymc.extreloff = 0;
+  dsymc.nextrel = 0;
+  dsymc.locreloff = 0;
+  dsymc.nlocrel = 0;
+
+  // Symbol Table entries
+  nl[0].n_un.n_strx = 0; // Character offset into the string table
+  nl[0].n_type = N_EXT | N_SECT;
+  nl[0].n_sect = 1;
+  nl[0].n_desc = REFERENCE_FLAG_DEFINED | REFERENCED_DYNAMICALLY | N_NO_DEAD_STRIP;
+  nl[0].n_value = 0; // Beginning addr symbol (beginning of section, will be relocated on link/load)
+
+  nl[1].n_un.n_strx = len+sfx_len+2; // Character offset into the string table
+  nl[1].n_type = N_EXT | N_SECT;
+  nl[1].n_sect = 1;
+  nl[1].n_desc = REFERENCE_FLAG_DEFINED | REFERENCED_DYNAMICALLY | N_NO_DEAD_STRIP;
+  nl[1].n_value = dsize; // Ending addr symbol (end of section, will be relocated on link/load)
+
+  // Allocate strings
+  fname_fixed = (char*)malloc(len+1);
+  ofname = (char*)malloc(len+3);
+  sym_beg = (char*)malloc(len+sfx_len+2);
+  sym_end = (char*)malloc(len+sfx_len+2);
+
+  // Manipulate filename for output filename and symbols
+  strcpy(fname_fixed, fname);
+  fix_fname(fname_fixed);
+  strcpy(ofname, fname_fixed);
+  strcat(ofname, ".o");
+  strcpy(sym_beg, "_"); // Mach-O dlsym() adds an '_' when performing lookup
+  strcat(sym_beg, fname_fixed);
+  strcat(sym_beg, beg_sfx);
+  strcpy(sym_end, "_"); // Mach-O dlsym() adds an '_' when performing lookup
+  strcat(sym_end, fname_fixed);
+  strcat(sym_end, end_sfx);
+
+  // Write the Mach-O object
+  objfile = fopen(ofname, "w");
+  fwrite(&mh, sizeof(mh), 1, objfile);
+  fwrite(&segc, sizeof(segc), 1, objfile);
+  fwrite(&sec, sizeof(sec), 1, objfile);
+  fwrite(&symc, sizeof(symc), 1, objfile);
+  fwrite(&dsymc, sizeof(dsymc), 1, objfile);
+  fwrite(data, dsize, 1, objfile);
+  fwrite(padding, pad_len(dsize), 1, objfile); // Pad data to word alignment
+  fwrite(nl, NUM_SYMBOLS*sizeof(struct nlist_64), 1, objfile);
+  fwrite(sym_beg, strlen(sym_beg) + 1, 1, objfile);
+  fwrite(sym_end, strlen(sym_end) + 1, 1, objfile);
+  fclose(objfile);
+
+  // Free string allocations
+  free(fname_fixed);
+  free(sym_beg);
+  free(sym_end);
+
+  return ofname; // Must be freed externally
+}
+
 #else // #ifdef DARWIN
 
-char *write_elf(char *fname, int dsize, char *data){
+char *write_elf32(char *fname, int dsize, char *data){
   Elf32_Ehdr eh;
-  //Elf64_Ehdr eh64;
   Elf32_Shdr sh[5];
-  //const char shstrtab[] = "\0.symtab\0.strtab\0.shstrtab\0.data\0";
   const char shstrtab[] = "\0.data\0.shstrtab\0.symtab\0.strtab\0\0\0\0";
   Elf32_Sym sym[NUM_SYMBOLS + 2]; // empty and SECTION symbols
   // Other vars
@@ -400,15 +532,12 @@ char *write_elf(char *fname, int dsize, char *data){
   eh.e_ident[EI_MAG2] = ELFMAG2;
   eh.e_ident[EI_MAG3] = ELFMAG3;
   eh.e_ident[EI_CLASS] = ELFCLASS32;
-  //eh.e_ident[EI_CLASS] = ELFCLASS64;
   eh.e_ident[EI_DATA] = ELFDATA2LSB;
   eh.e_ident[EI_VERSION] = EV_CURRENT;
   
   eh.e_type = ET_REL;
   eh.e_machine = EM_386;
-  //eh.e_machine = EM_X86_64;
   eh.e_version = EV_CURRENT;
-  //eh.e_shoff = 0; // GENERATED below
   eh.e_ehsize = sizeof(eh);
   eh.e_shentsize = sizeof(Elf32_Shdr);
   eh.e_shnum = 5;
@@ -464,9 +593,7 @@ char *write_elf(char *fname, int dsize, char *data){
   objfile = fopen(ofname, "w");
   fwrite(&eh, sizeof(eh), 1, objfile);
   fwrite(data, sh[1].sh_size, 1, objfile);
-  //fwrite(padding, pad_len(sh[1].sh_size), 1, objfile); // Pad data to word alignment
   fwrite(shstrtab, sh[2].sh_size, 1, objfile);
-  //fwrite(padding, pad_len(sh[2].sh_size), 1, objfile); // Pad string table to word alignment
   fwrite(sh, eh.e_shnum*eh.e_shentsize, 1, objfile);
   fwrite(sym, sh[3].sh_size, 1, objfile);
   fwrite(padding, 1, 1, objfile); // Empty string at beginning of strtab
@@ -482,6 +609,117 @@ char *write_elf(char *fname, int dsize, char *data){
   return ofname; // Must be freed externally
 }
 
+char *write_elf64(char *fname, int dsize, char *data){
+  Elf64_Ehdr eh;
+  Elf64_Shdr sh[5];
+  const char shstrtab[] = "\0.data\0.shstrtab\0.symtab\0.strtab\0\0\0\0";
+  Elf64_Sym sym[NUM_SYMBOLS + 2]; // empty and SECTION symbols
+  // Other vars
+  FILE *objfile;
+  char *fname_fixed;
+  char *ofname;
+  char *sym_beg;
+  char *sym_end;
+  int len = strlen(fname);
+
+  // Allocate strings
+  fname_fixed = (char*)malloc(len+1);
+  ofname = (char*)malloc(len+3);
+  sym_beg = (char*)malloc(len+sfx_len+1);
+  sym_end = (char*)malloc(len+sfx_len+1);
+
+  // Manipulate filename for output filename and symbols
+  strcpy(fname_fixed, fname);
+  fix_fname(fname_fixed);
+  strcpy(ofname, fname_fixed);
+  strcat(ofname, ".o");
+  strcpy(sym_beg, fname_fixed);
+  strcat(sym_beg, beg_sfx);
+  strcpy(sym_end, fname_fixed);
+  strcat(sym_end, end_sfx);
+
+  bzero(&eh, sizeof(eh));
+  eh.e_ident[EI_MAG0] = ELFMAG0;
+  eh.e_ident[EI_MAG1] = ELFMAG1;
+  eh.e_ident[EI_MAG2] = ELFMAG2;
+  eh.e_ident[EI_MAG3] = ELFMAG3;
+  eh.e_ident[EI_CLASS] = ELFCLASS64;
+  eh.e_ident[EI_DATA] = ELFDATA2LSB;
+  eh.e_ident[EI_VERSION] = EV_CURRENT;
+  
+  eh.e_type = ET_REL;
+  eh.e_machine = EM_X86_64;
+  eh.e_version = EV_CURRENT;
+  eh.e_ehsize = sizeof(eh);
+  eh.e_shentsize = sizeof(Elf64_Shdr);
+  eh.e_shnum = 5;
+  eh.e_shstrndx = 2;
+
+  bzero(sh, 5*sizeof(Elf64_Shdr));
+  sh[0].sh_type = SHT_NULL;
+
+  sh[1].sh_name = strlen(&shstrtab[sh[0].sh_name])+sh[0].sh_name+1;
+  sh[1].sh_type = SHT_PROGBITS;
+  sh[1].sh_flags = SHF_ALLOC; // Memory mapped, but not writeable
+  sh[1].sh_offset = eh.e_ehsize;
+  sh[1].sh_size = round_to_word(dsize); // GENERATED
+  sh[1].sh_addralign = 4;
+
+  sh[2].sh_name = strlen(&shstrtab[sh[1].sh_name])+sh[1].sh_name+1;
+  sh[2].sh_type = SHT_STRTAB;
+  sh[2].sh_offset = sh[1].sh_offset + sh[1].sh_size; // GENERATED
+  sh[2].sh_size = 36;
+  sh[2].sh_addralign = 4;
+
+  eh.e_shoff = round_to_word(sh[2].sh_offset + sh[2].sh_size); // GENERATED
+
+  sh[3].sh_name = strlen(&shstrtab[sh[2].sh_name])+sh[2].sh_name+1;
+  sh[3].sh_type = SHT_SYMTAB;
+  sh[3].sh_offset = eh.e_shoff + eh.e_shentsize * eh.e_shnum;
+  sh[3].sh_size = (NUM_SYMBOLS + 2) * sizeof(Elf64_Sym);
+  sh[3].sh_link = 4;
+  sh[3].sh_info = 2;
+  sh[3].sh_addralign = 4;
+  sh[3].sh_entsize = sizeof(Elf64_Sym);
+
+  sh[4].sh_name = strlen(&shstrtab[sh[3].sh_name])+sh[3].sh_name+1;
+  sh[4].sh_type = SHT_STRTAB;
+  sh[4].sh_offset = sh[3].sh_offset + sh[3].sh_size;
+  sh[4].sh_size = strlen(sym_beg) + strlen(sym_end) + 3; // GENERATED
+  sh[4].sh_addralign = 1;
+
+  bzero(sym, (NUM_SYMBOLS + 2)*sizeof(Elf64_Sym));
+  sym[1].st_info = ELF64_ST_INFO(STB_LOCAL, STT_SECTION);
+  sym[1].st_shndx = 1;
+
+  sym[2].st_name = 1;
+  sym[2].st_info = ELF64_ST_INFO(STB_GLOBAL, STT_NOTYPE);
+  sym[2].st_shndx = 1;
+
+  sym[3].st_name = strlen(sym_beg)+2;
+  sym[3].st_value = dsize;
+  sym[3].st_info = ELF64_ST_INFO(STB_GLOBAL, STT_NOTYPE);
+  sym[3].st_shndx = 1;
+
+  // Write the ELF object
+  objfile = fopen(ofname, "w");
+  fwrite(&eh, sizeof(eh), 1, objfile);
+  fwrite(data, sh[1].sh_size, 1, objfile);
+  fwrite(shstrtab, sh[2].sh_size, 1, objfile);
+  fwrite(sh, eh.e_shnum*eh.e_shentsize, 1, objfile);
+  fwrite(sym, sh[3].sh_size, 1, objfile);
+  fwrite(padding, 1, 1, objfile); // Empty string at beginning of strtab
+  fwrite(sym_beg, strlen(sym_beg)+1, 1, objfile);
+  fwrite(sym_end, strlen(sym_end)+1, 1, objfile);
+  fclose(objfile);
+
+  // Free string allocations
+  free(fname_fixed);
+  free(sym_beg);
+  free(sym_end);
+
+  return ofname; // Must be freed externally
+}
 #endif // #ifdef DARWIN
 
 unsigned long read_binary_file(const char *fname, unsigned char **data){
@@ -535,9 +773,17 @@ void write_obj_files(int numfiles, char **fnames){
     if(dsize){
       compress_data(data,dsize,&cdata,&cdsize);
 #ifdef DARWIN
-      ofname = write_mach_o(fnames[i], cdsize, cdata);
+      // Write objects for the same architecture that simlib was compiled for (32bit/64bit)
+      if(4 == sizeof(void*))
+	ofname = write_mach_o32(fnames[i], cdsize, cdata);
+      else
+	ofname = write_mach_o64(fnames[i], cdsize, cdata);
 #else
-      ofname = write_elf(fnames[i], cdsize, cdata);
+      // Write objects for the same architecture that simlib was compiled for (32bit/64bit)
+      if(4 == sizeof(void*))
+	ofname = write_elf32(fnames[i], cdsize, cdata);
+      else
+	ofname = write_elf64(fnames[i], cdsize, cdata);
 #endif
       printf("%s\n", ofname);
       free(ofname);
@@ -548,13 +794,13 @@ void write_obj_files(int numfiles, char **fnames){
   }
 }
 
-void sim_extract_files(char *library, int numfiles, char **fnames){
+int sim_extract_files(char *library, int numfiles, char **fnames){
   int i;
   unsigned char *cdata, *data;
   int cdsize, dsize;
 
   if(!open_binary(library))
-    return;
+    return 1;
 
   // Extract all symbols
   for(i=0;i<numfiles;i++){
@@ -568,6 +814,7 @@ void sim_extract_files(char *library, int numfiles, char **fnames){
   // cdata need not be freed as it points to constant memory in the library
   close_binary();
 
+  return 0;
 }
 
 // This program takes the following parameters on the command line
