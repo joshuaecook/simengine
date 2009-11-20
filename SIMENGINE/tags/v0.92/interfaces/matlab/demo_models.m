8ecbff5e64a032044d440cb714c24ddd4aa5825e
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-11-20 22:56:30 +0000

    Fixed demo_models, changed Makefile to better work with 64-bit builds on a Mac

--- SIMENGINE/tags/v0.92/interfaces/matlab/demo_models.m
+++ SIMENGINE/tags/v0.92/interfaces/matlab/demo_models.m
@@ -7,7 +7,7 @@ demo = struct();
 
 % start with a brk model
 demo(i).title = 'BRK - Spike Train';
-demo(i).file = [simexamplepath '/brk.dsl'];
+demo(i).file = [simexamplepath '/BRK/brk.dsl'];
 demo(i).starttime = 0;
 demo(i).stoptime = 100;
 demo(i).inputs.Iext = [0 40];
@@ -29,7 +29,7 @@ demo(i).target = 'parallel-cpu';
 i = i + 1;
 % next, try FN
 demo(i).title = 'FN - Sweep Current';
-demo(i).file = [simexamplepath '/fn.dsl'];
+demo(i).file = [simexamplepath '/FN/fn.dsl'];
 demo(i).starttime = 0;
 demo(i).stoptime = 100;
 demo(i).inputs.I = [0 4];
