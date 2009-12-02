6327776577b59b11116e04081b1d3c0bd92f56c5
clebsack <clebsack@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-12-02 21:15:49 +0000

    Changed evaluation of active models for the GPU.

--- SIMENGINE/branches/McIntyre_gpu/codegen/src/simengine/exec_parallel_gpu.cu
+++ SIMENGINE/branches/McIntyre_gpu/codegen/src/simengine/exec_parallel_gpu.cu
@@ -7,11 +7,11 @@ int exec_parallel_gpu(solver_props *props){
   unsigned int active_models;
   solver_props *device_props;
 
-  active_models = NUM_MODELS;
   num_gpu_threads = GPU_BLOCK_SIZE < NUM_MODELS ? GPU_BLOCK_SIZE : NUM_MODELS;
   num_gpu_blocks = (NUM_MODELS + GPU_BLOCK_SIZE - 1) / GPU_BLOCK_SIZE;
 
   // Initialize all iterators to running
+  active_models = 1;
   for(modelid = 0; modelid < NUM_MODELS; modelid++){
     for(i=0;i<NUM_ITERATORS;i++){
       iter = ITERATORS[i];
@@ -28,11 +28,12 @@ int exec_parallel_gpu(solver_props *props){
     // Copy data back to the host
     cutilSafeCall(cudaMemcpy(props->ob, props->gpu.ob, props->ob_size, cudaMemcpyDeviceToHost));
 
+    active_models = 0;
     // Copy data in parallel to external api interface
     for(modelid = 0; modelid < props->num_models; modelid++){
       if(0 != log_outputs(props->ob, props->outputs, modelid)) return ERRMEM;
-      
-      if (props->ob->finished[modelid]) active_models--;
+
+      active_models |= !props->ob->finished[modelid];
     }
   }
 
