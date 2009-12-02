9840b4193781a77d3a92bd44bd0eb0a14f517b30
clebsack <clebsack@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-12-02 17:51:14 +0000

    Fixed parallel linearbackwardeuler indexing.

--- SIMENGINE/branches/McIntyre_gpu/codegen/src/solvers/linearbackwardeuler.c
+++ SIMENGINE/branches/McIntyre_gpu/codegen/src/solvers/linearbackwardeuler.c
@@ -1,18 +1,6 @@
 
 __HOST__ __DEVICE__
-int SPIDX(int n, int hbw,int r, int c, unsigned int num_models, unsigned int modelid){
-  int idx = TARGET_IDX(n*(hbw*2+1), num_models, r*(hbw*2+1)+c, modelid);
-  return idx;
-} /* Row major ordering - Sparse */
-
-__HOST__ __DEVICE__
-int DIDX(int n,int r,int c, unsigned int num_models, unsigned int modelid){
-  int idx = TARGET_IDX(n*n, num_models, r*n+c, modelid);
-  return idx;
-} /* Row major ordering - Dense */
-
-__HOST__ __DEVICE__
-int MATIDX(int nc, int nr, int c, int r, unsigned int num_models, unsigned int modelid){
+int MATIDX(int nr, int nc, int r, int c, unsigned int num_models, unsigned int modelid){
   int idx = TARGET_IDX(nc*nr, num_models, r*nc+c, modelid);
   return idx;
 }
@@ -47,7 +35,6 @@ int linearbackwardeuler_init(solver_props *props){
 __DEVICE__
 int linearbackwardeuler_eval(solver_props *props, unsigned int modelid){
   CDATAFORMAT* M = (CDATAFORMAT *)props->mem;
-  int hbw = props->bandsize>>1; // Integer division by 2
 
   // Stop the simulation if the next step will be beyond the stoptime (this will hit the stoptime exactly for even multiples unless there is rounding error)
   props->running[modelid] = props->time[modelid] + props->timestep <= props->stoptime;
@@ -56,7 +43,7 @@ int linearbackwardeuler_eval(solver_props *props, unsigned int modelid){
 
   int ret = model_flows(props->time[modelid], props->model_states, props->next_states/*b_x*/, props, 1, modelid);
 
-  bandsolv_sparse(props->statesize, hbw, M, props->next_states, props->num_models, modelid);
+  bandsolv_sparse(props->statesize, props->bandsize, M, props->next_states, props->num_models, modelid);
 
   props->next_time[modelid] += props->timestep;
 
@@ -83,51 +70,53 @@ int linearbackwardeuler_free(solver_props *props){
  */
 
 __DEVICE__
-void bandsolv_dense(const int n, const int hbw, CDATAFORMAT *M, CDATAFORMAT *b_x, unsigned int num_models, unsigned int modelid){
+void bandsolv_dense(const int n, const int bw, CDATAFORMAT *M, CDATAFORMAT *b_x, unsigned int num_models, unsigned int modelid){
   CDATAFORMAT rmult; /* Constant multiplier for a row when eliminating a value from another row */
   int br, er, bc, ec; /* Row and column matrix indicies */
   int idx; /* Vector index */
+  int hbw = bw>>1; // Integer division by 2
 
   /* Clear out lower half of M */
   for(br = bc = 0; br < n-1; br++, bc++){
     for(er = br + 1; er <= MIN(br + hbw, n-1); er++){
-      rmult = -M[DIDX(n,er,bc, num_models, modelid)]/M[DIDX(n,br,bc, num_models, modelid)];
+      rmult = -M[MATIDX(n,n,er,bc, num_models, modelid)]/M[MATIDX(n,n,br,bc, num_models, modelid)];
       for(ec = bc + 1; ec <= MIN(bc + hbw, n); ec++){
-	M[DIDX(n,er,ec, num_models, modelid)] += rmult*M[DIDX(n,br,ec, num_models, modelid)];
+	M[MATIDX(n,n,er,ec, num_models, modelid)] += rmult*M[MATIDX(n,n,br,ec, num_models, modelid)];
       }
-      b_x[er] += rmult*b_x[br];
+      b_x[VECIDX(n,er,num_models,modelid)] += rmult*b_x[VECIDX(n,br,num_models,modelid)];
     }
   }
 
   /* Clear out upper half of M */
   for(br = bc = n-1; br > 0; br--, bc--){
     for(er = br - 1; er >= MAX(br - hbw, 0); er--){
-      rmult = -M[DIDX(n,er,bc, num_models, modelid)]/M[DIDX(n,br,bc, num_models, modelid)];
-      b_x[er] += rmult*b_x[br];
+      rmult = -M[MATIDX(n,n,er,bc, num_models, modelid)]/M[MATIDX(n,n,br,bc, num_models, modelid)];
+      b_x[VECIDX(n,er,num_models,modelid)] += rmult*b_x[VECIDX(n,br,num_models,modelid)];
     }
   }
 
   /* Produce x from remaining diagonal of M */
   for(idx = 0; idx < n; idx++){
-    b_x[idx] = b_x[idx]/M[DIDX(n,idx,idx, num_models, modelid)];
+    b_x[VECIDX(n,idx,num_models,modelid)] = b_x[VECIDX(n,idx,num_models,modelid)]/M[MATIDX(n,n,idx,idx, num_models, modelid)];
   }
 }
 
 __DEVICE__
-void bandsolv_sparse(const int n, const int hbw, CDATAFORMAT *M, CDATAFORMAT *b_x, unsigned int num_models, unsigned int modelid){
+void bandsolv_sparse(const int n, const int bw, CDATAFORMAT *M, CDATAFORMAT *b_x, unsigned int num_models, unsigned int modelid){
   CDATAFORMAT rmult; /* Constant multiplier for a row when eliminating a value from another row */
   int br, er, bc, ec; /* Row and column matrix indicies */
+  int hbw = bw>>1; // Integer division by 2
 
   /* Clear out lower half of M */
   for(br = 0; br < n-1; br++){
     for(er = br + 1; er <= MIN(br + hbw, n-1); er++){
       bc = hbw;
       ec = bc - (er-br);
-      rmult = -M[SPIDX(n,hbw,er,ec, num_models, modelid)]/M[SPIDX(n,hbw,br,bc, num_models, modelid)];
+      rmult = -M[MATIDX(n,bw,er,ec, num_models, modelid)]/M[MATIDX(n,bw,br,bc, num_models, modelid)];
       for(; bc < 2*hbw+1; ec++, bc++){
-	M[SPIDX(n,hbw,er,ec, num_models, modelid)] += rmult*M[SPIDX(n,hbw,br,bc, num_models, modelid)];
+	M[MATIDX(n,bw,er,ec, num_models, modelid)] += rmult*M[MATIDX(n,bw,br,bc, num_models, modelid)];
       }
-      b_x[er] += rmult*b_x[br];
+      b_x[VECIDX(n,er,num_models,modelid)] += rmult*b_x[VECIDX(n,br,num_models,modelid)];
     }
   }
 
@@ -136,13 +125,13 @@ void bandsolv_sparse(const int n, const int hbw, CDATAFORMAT *M, CDATAFORMAT *b_
   for(br = n-1; br > 0; br--){
     for(er = br - 1; er >= MAX(br - hbw, 0); er--){
       ec = bc + (br-er);
-      rmult = -M[SPIDX(n,hbw,er,ec, num_models, modelid)]/M[SPIDX(n,hbw,br,bc, num_models, modelid)];
-      b_x[er] += rmult*b_x[br];
+      rmult = -M[MATIDX(n,bw,er,ec, num_models, modelid)]/M[MATIDX(n,bw,br,bc, num_models, modelid)];
+      b_x[VECIDX(n,er,num_models,modelid)] += rmult*b_x[VECIDX(n,br,num_models,modelid)];
     }
   }
 
   /* Produce x from remaining diagonal of M */
   for(br = 0; br < n; br++){
-    b_x[br] = b_x[br]/M[SPIDX(n,hbw,br,bc, num_models, modelid)];
+    b_x[VECIDX(n,br,num_models,modelid)] = b_x[VECIDX(n,br,num_models,modelid)]/M[MATIDX(n,bw,br,bc, num_models, modelid)];
   }
 }
