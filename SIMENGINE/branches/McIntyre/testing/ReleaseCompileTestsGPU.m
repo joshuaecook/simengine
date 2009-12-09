c7f365e5a39d160adf551b1742ec2d54bf4345b3
clebsack <clebsack@53642c02-b353-73e9-57f2-aeda9b58858d>
2009-12-09 21:44:57 +0000

    Forgot new GPU test files

---
+++ SIMENGINE/branches/McIntyre/testing/ReleaseCompileTestsGPU.m
@@ -0,0 +1,38 @@
+function s = ReleaseCompileTestsGPU
+
+% grab the buildEngine path
+buildenginepath = which('simex');
+[installpath,filename,ext] = fileparts(buildenginepath);
+
+% determine the example path
+examplepath = fullfile(installpath, 'examples');
+
+dsl_files_str = ls('-1',fullfile(examplepath,'/*/*.dsl'));
+dsl_files = strread(dsl_files_str, '%s', 'delimiter', sprintf('\n'));
+
+% create a suite of tests
+s = Suite('Release Compile Tests GPU');
+
+% add each of the dsl files to a run script
+for i=1:length(dsl_files)
+    [path, name, ext] = fileparts(dsl_files{i});
+    if name(1) ~= '.'
+        filename = fullfile(path, [name ext]);
+        s.add(Test(['Model-' name], @()(simex(filename)), '-withouterror'));
+    end
+end
+
+% The below test won't pass because the model name is different than the
+% file name.  This is expected
+s.getTest('Model-neuronWithSynapse').ExpectFail = true;
+
+% These tests use cvode and we should add a compiler error message to check against, but for now, just expect them to fail on the GPU
+s.getTest('Model-timingNetwork').ExpectFail = true;
+s.getTest('Model-lorenz').ExpectFail = true;
+s.getTest('Model-axon').ExpectFail = true;
+s.getTest('Model-flut').ExpectFail = true;
+s.getTest('Model-node').ExpectFail = true;
+s.getTest('Model-segment').ExpectFail = true;
+s.getTest('Model-purine').ExpectFail = true;
+
+end
