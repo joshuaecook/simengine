68cbadca991a2fdd12b22a24a570b562202e6405
cchurch <cchurch@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-04-15 15:33:48 +0000

    added comments to solar system model matlab scripts

--- SIMENGINE/trunk/ReleaseExamples/demos/solarsystem/innersystem.m
+++ SIMENGINE/trunk/ReleaseExamples/demos/solarsystem/innersystem.m
@@ -1,11 +1,8 @@
+% INNERSYSTEM - displays planets as circles moving in their orbital paths
+%
+% Copyright 2010 Simatra Modeling Technologies
 function o = innersystem
 o = simex('innersystem.dsl', 5e7);
-
-%paddle_length = 6;
-%paddle_width = 2;
-%half = paddle_length/2;
-%x_p1 = 3;
-%x_p2 = 97;
 
 dims = [-2.5e8 -2.5e8 5e8 5e8];
 
@@ -13,29 +10,14 @@ axis off;
 
 rectangle('Position', dims, 'FaceColor', 'Black');
 
-%ball = @(x,y)([x-1 y-1 200/100 60/100]);
-
-%paddle1 = [x_p1-1 0 paddle_width paddle_length];
-%paddle2 = [x_p2-1 0 paddle_width paddle_length];
-
-
-
 for i=1:length(o.sundata(:,1))
     pause(0.002)
     
     % Recreate background
     clf % clear the figure
     rectangle('Position', dims, 'FaceColor', 'Black');
-%    line([50 50], [0 30], 'LineStyle', '--', 'LineWidth', 10, 'Color', 'Green');    
-    
-    % Add player scores
-%    text(3,28, num2str(int32(o.scores(i,2))), 'Color', 'Green', 'FontSize', 20, 'FontName', 'Courier');
-%    text(96,28, num2str(int32(o.scores(i,3))), 'Color', 'Green', 'FontSize', 20, 'FontName', 'Courier');    
 
-    % Add count down
-%    text(95,1, num2str(floor(o.ball(end,1)-o.ball(i,1))), 'Color', 'Green', 'FontSize', 20, 'FontName', 'Courier');
-    
-    % Add ball
+    % Add planets
     line(o.sundata(i,2),o.sundata(i,3), 'Marker', '.', 'MarkerSize', 30, 'Color', 'Yellow');
     line(o.earthdata(i,2),o.earthdata(i,3), 'Marker', '.', 'MarkerSize', 3, 'Color', 'Cyan');
 
@@ -48,14 +30,7 @@ for i=1:length(o.sundata(:,1))
     line(o.phobosdata(i,2),o.deimosdata(i,3), 'Marker', '.', 'MarkerSize', 1, 'Color', [0.7 0.7 0.7]);
 
     axis([-2.5e8 2.5e8 -2.5e8 2.5e8])
-%     axis([0 2.5e8 0 2.5e8])
     axis off
     
-    % draw paddle 1
-%    paddle1(2) = o.paddle1(i,2)-half;
-%    rectangle('Position', paddle1, 'FaceColor', 'Green');
-%    % draw paddle 2
-%    paddle2(2) = o.paddle2(i,2)-half;
-%    rectangle('Position', paddle2, 'FaceColor', 'Green');
 end
 end
--- SIMENGINE/trunk/ReleaseExamples/demos/solarsystem/solarsystem.m
+++ SIMENGINE/trunk/ReleaseExamples/demos/solarsystem/solarsystem.m
@@ -1,3 +1,6 @@
+% SOLARSYSTEM - Shows orbital paths of planets (and our moon) around the sun
+%
+% Copyright 2010 Simatra Modeling Technologies
 function solarsystem
   o = simex('solarsystem.dsl', 1e8);
   simplot(o.moondata(2:end,2:3), o.earthdata(2:end,2:3), o.sundata(2:end,2:3), o.marsdata(2:end,2:3), o.venusdata(2:end,2:3), o.mercurydata(2:end,2:3), o.jupiterdata(2:end,2:3), o.saturndata(2:end,2:3), o.uranusdata(2:end,2:3), o.neptunedata(2:end,2:3));
