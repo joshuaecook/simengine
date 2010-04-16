b052e6d8e21cdd976ce3c8d7a8bb0f52c6072ac3
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-04-16 19:16:43 +0000

    Updated rossler demo and moved synth to examples

--- SIMENGINE/trunk/ReleaseExamples/demos/rossler/rossler.m
+++ SIMENGINE/trunk/ReleaseExamples/demos/rossler/rossler.m
@@ -1,9 +1,25 @@
+% ROSSLER attractor demo model in simEngine
+%
+% Copyright 2010 Simatra Modeling Technologies
+%
 function rossler
-  o = simex('rossler.dsl', 1e3);
-  figure;
-  simplot(o.x(:,2), o.y(:,2));
-  figure;
-  simplot(o.x(:,2), o.z(:,2));
-  figure;
-  simplot(o.y(:,2), o.z(:,2));
+
+% Simulate the rossler model using simEngine, set the stop time to 1000
+o = simex('rossler.dsl', 1000);
+
+% Create four subplots, the first three being 2D and the final one
+% being a 3D plot
+subplot(2,2,1)
+simplot(o.x(:,2),o.y(:,2))
+title('Rossler x vs. y')
+subplot(2,2,2)
+simplot(o.y(:,2),o.z(:,2))
+title('Rossler y vs. z')
+subplot(2,2,3)
+simplot(o.x(:,2),o.z(:,2))
+title('Rossler x vs. z')
+subplot(2,2,4)
+plot3(o.x(:,2),o.y(:,2),o.z(:,2))
+title('Rossler x vs. y vs. z')
+
 end
--- SIMENGINE/trunk/ReleaseExamples/demos/synth/lfo.dsl
+++
@@ -1,8 +0,0 @@
-//Copyright (C) 2010 by Simatra Modeling Technologies, L.L.C.
-
-model (y) = lfo (w, phi)
-
-equation r = 2 * t * pi
-output y = sin(w * r + phi)
-
-end
