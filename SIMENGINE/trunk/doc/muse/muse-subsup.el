a15a15d98924117fdfb1e0ad2ed06737c308cdc7
jcook <jcook@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-04-27 14:57:55 +0000

    An extension for MUSE for subscript/superscript text.

---
+++ SIMENGINE/trunk/doc/muse/muse-subsup.el
@@ -0,0 +1,62 @@
+; muse-subsup.el Adds superscript and subscript to MUSE documents
+; Copyright (C) 2010 by Simatra Modeling Technologies, L.L.C.
+
+(require 'muse-publish)
+
+(defgroup muse-subsup nil
+  "Superscript and subscript for MUSE documents.")
+
+(defun muse-subsup-sup-tag (begin end &optional attr)
+  "Invoked by MUSE when encountering a <sup> tag."
+  (save-restriction
+    (narrow-to-region begin end)
+    (goto-char (point-max))
+    (when muse-publishing-p
+      (muse-insert-markup
+       (cond
+	((or (muse-style-derived-p "latex") (muse-style-derived-p "context"))
+	 "}")
+	((or (muse-style-derived-p "html") (muse-style-derived-p "xml"))
+	 "</sup>")))
+      (goto-char (point-min))
+      (muse-insert-markup
+       (cond
+	((or (muse-style-derived-p "latex") (muse-style-derived-p "context"))
+	 "^{")
+	((or (muse-style-derived-p "html") (muse-style-derived-p "xml"))
+	 "<sup>")))
+      (goto-char (point-max)))))
+
+(defun muse-subsup-sub-tag (begin end &optional attr)
+  "Invoked by MUSE when encountering a <sub> tag."
+  (save-restriction
+    (narrow-to-region begin end)
+    (goto-char (point-max))
+    (when muse-publishing-p
+      (muse-insert-markup
+       (cond
+	((or (muse-style-derived-p "latex") (muse-style-derived-p "context"))
+	 "}")
+	((or (muse-style-derived-p "html") (muse-style-derived-p "xml"))
+	 "</sub>")))
+      (goto-char (point-min))
+      (muse-insert-markup
+       (cond
+	((or (muse-style-derived-p "latex") (muse-style-derived-p "context"))
+	 "_{")
+	((or (muse-style-derived-p "html") (muse-style-derived-p "xml"))
+	 "<sub>")))
+      (goto-char (point-max)))))
+
+;; Hooks into publish
+(add-to-list 'muse-publish-markup-tags
+	     '("sub" t nil t muse-subsup-sub-tag)
+	     t)
+
+(add-to-list 'muse-publish-markup-tags
+	     '("sup" t nil t muse-subsup-sup-tag)
+	     t)
+
+
+(provide 'muse-subsup)
+;;;; muse-subsup.el
