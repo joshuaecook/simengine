efef06065d1a9dbc3218fde1174a37e9dfb44044
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-07-28 13:06:32 +0000

    Cleaned up and improved createMRGAxon

--- SIMENGINE/trunk/interfaces/matlab/dsl_gen/dslgen/MRG/createMRGAxon.m
+++ SIMENGINE/trunk/interfaces/matlab/dsl_gen/dslgen/MRG/createMRGAxon.m
@@ -1,9 +1,5 @@
 function m = createMRGAxon(numSegments)
 
-% Store the PWD - would like a workaround for this
-[file_path] = fileparts(which('createMRGAxon'));
-
-
 % First define the global iterators
 dt = 0.001;
 t_imp = Iterator('t_imp', 'solver', 'linearbackwardeuler', 'dt', dt);
@@ -33,7 +29,6 @@ end
 Vext{end} = m.input(['Vext' num2str(numSegments+1)],0);
 
 % Now instantiate the segment submodels
-segment_dsl = fullfile(file_path, 'segment.dsl');
 segments = cell(1,numSegments);
 for i=1:numSegments
     segments{i} = m.submodel(m_segment);
@@ -43,7 +38,6 @@ for i=1:numSegments
 end
 
 % Add a final node
-node_dsl = fullfile(file_path, 'node.dsl');
 node = m.submodel(m_node);
 node.Vext = Vext{end};
 
@@ -96,19 +90,7 @@ else
 end
 m.output('Vm', outputList);
 
-% Create helper function to reduce a cell array of boolean expressions to
-% one expression - replace with the ANY command when vectorized.
-    function er = reduction_or(cell_list)
-        if ~isempty(cell_list)
-            er = cell_list{1};
-            for i=2:length(cell_list)
-                er = er | cell_list{i};
-            end
-        else
-            er = Exp(1);
-        end
-    end
-m.output('spikes', spikes, 'when', reduction_or(spikes));
+m.output('spikes', spikes, 'when', any(spikes{:}));
 
 end
 
