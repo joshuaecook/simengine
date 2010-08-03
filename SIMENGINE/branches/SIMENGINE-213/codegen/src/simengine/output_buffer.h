78bcd3fb28fb356bcefd5322dbda8b84c3962d3a
jcook <jcook@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-08-03 18:18:43 +0000

    Initial take on new data structures for indexed output buffer. SIMENGINE-213

--- SIMENGINE/branches/SIMENGINE-213/codegen/src/simengine/output_buffer.h
+++ SIMENGINE/branches/SIMENGINE-213/codegen/src/simengine/output_buffer.h
@@ -31,3 +31,69 @@ typedef struct {
   unsigned int num_quantities;
   CDATAFORMAT quantities[];
 } output_buffer_data;
+
+
+/* A marker of the sort index. */
+typedef struct {
+  unsigned int modelid;
+  unsigned int outputid;
+  unsigned int offset;
+} indexed_sort_data;
+
+/* An indexed element array.
+ * The buffer contains packed output data, and the
+ * sort array comprises a set of markers identifying
+ * the position of each datum.
+ */
+typedef struct {
+  size_t size;
+  CDATAFORMAT buffer[BUFFER_LEN];
+  indexed_sort_data sort[2*BUFFER_LEN];
+} indexed_output_buffer;
+
+
+
+/* Destructively computes the prefix sum of an integer vector of size length.
+ * Assumes length is a power of 2. 
+ */
+__DEVICE__ void parallel_scan(int *vector, unsigned int threadid, unsigned int length) {
+  int i,stride;
+  int participate;
+
+  for (stride=2; stride<=length; stride*=2) {
+    participate = !((threadid+1) & (stride-1));
+    if (participate) {
+      vector[threadid] += vector[threadid-(stride/2)];
+    }
+  }
+}
+
+/* Writes an output datum to an indexed buffer. 
+ * Operates on blocksize model instances in parallel.
+ */
+__DEVICE__ void buffer_indexed_ouput (unsigned int modelid, unsigned int outputid, size_t outputsize, CDATAFORMAT *quantities, indexed_output_buffer *pos, unsigned int threadid, unsigned int blocksize, int *participate, int *index) {
+  int i, offset;
+  CDATAFORMAT *buffer;
+  indexed_sort_data *sort;
+
+  index[threadid] = !!participate[threadid];
+  parallel_scan(index,threadid,blocksize);
+
+  if (participate[threadid]) {
+    offset = pos->size + index[threadid] - 1;
+
+    buffer = pos->buffer;
+    for (i=0; i<outputsize; i++) {
+      buffer[i+offset] = quantities[VEC_IDX(outputsize,i,blocksize,threadid)];
+    }
+
+    sort = pos->sort + offset;
+    sort->modelid = modelid;
+    sort->outputid = outputid;
+    sort->offset = offset;
+  }
+
+  if (0 == threadid) {
+    pos->size += index[blocksize-1] * outputsize;
+  }
+}
