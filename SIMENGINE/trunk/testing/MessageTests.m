5f545e2dc7fa6ea2393b36b391eb273580dcf87a
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-08-17 12:17:16 +0000

    Updated MessageTest to reflect 1.2

--- SIMENGINE/trunk/testing/MessageTests.m
+++ SIMENGINE/trunk/testing/MessageTests.m
@@ -18,7 +18,7 @@ end
 s = Suite('Message Tests');
 
 % Verify the version
-s.add(Test('CheckVersion', @()(simex('models_SolverTests/fn_forwardeuler.dsl')),'-regexpmatch', 'v1\.1'));
+s.add(Test('CheckVersion', @()(simex('models_SolverTests/fn_forwardeuler.dsl')),'-regexpmatch', 'v1\.2'));
 
 % Add basic variable tests
 s.add(SyntaxTests);
