55f9e095fb062f8f1c0536b83fc0df9fef1af298
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-09-16 18:17:31 +0000

    Made significant update to Iterator...  now supports options, such as reltol and abstol for ode23 and ode45, plus some cvode options.

--- SIMENGINE/trunk/ReleaseExamples/demos/rossler/rossler.m
+++ SIMENGINE/trunk/ReleaseExamples/demos/rossler/rossler.m
@@ -5,7 +5,7 @@
 function rossler
 
 % Simulate the rossler model using simEngine, set the stop time to 1000
-o = simex('rossler.dsl', 1000);
+o = simex(create_rossler, 1000);
 
 % Create four subplots, the first three being 2D and the final one
 % being a 3D plot
@@ -22,4 +22,36 @@ subplot(2,2,4)
 plot3(o.x(:,2),o.y(:,2),o.z(:,2))
 title('Rossler x vs. y vs. z')
 
+end
+
+function m = create_rossler
+
+% Create an iterator (solver) for the Rossler model with increased accuracy
+t = Iterator('continuous', 'solver', 'ode45');
+t.reltol = 1e-8;
+t.abstol = 1e-8;
+
+% Create the Rossler model object
+m = Model('rossler', t);
+
+% Define three scalar input parameters to the system
+a = m.input('a', 0.2);
+b = m.input('b', 0.2);
+c = m.input('c', 5.7);
+
+% Define each of the three state variables with their initial values equal
+% to zero
+x = m.state(0);
+y = m.state(0);
+z = m.state(0);
+
+% Define each of the three differential equations
+m.diffequ(x, -y - z);
+m.diffequ(y, x + a * y);
+m.diffequ(z, b + z * (x - c));
+
+% Create three outputs for each variable
+m.output(x);
+m.output(y);
+m.output(z);
 end
