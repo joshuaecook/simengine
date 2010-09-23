c38c29f0fbc72bdab3b461c7a17e931955f6676a
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-09-23 13:47:37 +0000

    Made a fix the check version test to make it work irrespective of the -quiet flag

--- SIMENGINE/trunk/testing/MessageTests.m
+++ SIMENGINE/trunk/testing/MessageTests.m
@@ -18,7 +18,7 @@ end
 s = Suite('Message Tests');
 
 % Verify the version
-s.add(Test('CheckVersion', @()(simex('models_SolverTests/fn_forwardeuler.dsl')),'-regexpmatch', 'v1\.2'));
+s.add(Test('CheckVersion', @()(simex('models_SolverTests/fn_forwardeuler.dsl', 1, '-quiet=false')),'-regexpmatch', 'v1\.2'));
 
 % Add basic variable tests
 s.add(SyntaxTests);
