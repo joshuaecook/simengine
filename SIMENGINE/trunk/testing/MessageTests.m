dd8e20ac3ef11959096b772e49c456b3dd0e1052
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-09-23 14:26:05 +0000

    Moved failing message tests from release tests to internal tests

--- SIMENGINE/trunk/testing/MessageTests.m
+++ SIMENGINE/trunk/testing/MessageTests.m
@@ -12,7 +12,14 @@ INTERNAL = 0; RELEASE = 1;
 if nargin == 0
     mode = RELEASE;
 else
-    mode = varargin{1};
+    switch lower(varargin{1})
+        case '-release'
+            mode = RELEASE;
+        case '-internal'
+            mode = INTERNAL;
+        otherwise
+            error('Simatra:MessageTests:ArgumentError', 'Invalid mode %s', varargin{1});
+    end
 end
 
 s = Suite('Message Tests');
@@ -21,17 +28,18 @@ s = Suite('Message Tests');
 s.add(Test('CheckVersion', @()(simex('models_SolverTests/fn_forwardeuler.dsl', 1, '-quiet=false')),'-regexpmatch', 'v1\.2'));
 
 % Add basic variable tests
-s.add(SyntaxTests);
-s.add(VariableTests);
-s.add(IteratorTests);
+s.add(SyntaxTests(mode));
+s.add(VariableTests(mode));
+s.add(IteratorTests(mode));
 % s.add(SolverTests);
-s.add(EquationTests);
-s.add(OutputTests);
+s.add(EquationTests(mode));
+s.add(OutputTests(mode));
 
 
 end
 
-function s = SyntaxTests
+function s = SyntaxTests(mode)
+INTERNAL = 0; RELEASE = 1;
 
 % Define a suite to check for errors found in the lexer/grammar
 s = Suite('Syntax Tests');
@@ -44,8 +52,10 @@ s.add(s_model);
 % Add the tests one by one
 s_model.add(CreateUserErrorTest('MispelledModel', 'ModelSyntaxTest1.dsl', ...
                                 'Unknown identifier encountered: mdel'));
-s_model.add(CreateUserErrorTest('NoModelEnd', 'ModelSyntaxTest2.dsl', ...
-                                'Adding END keyword'));
+if mode == INTERNAL
+    s_model.add(CreateUserErrorTest('NoModelEnd', 'ModelSyntaxTest2.dsl', ...
+                                    'Adding END keyword'));
+end
 s_model.add(CreateUserErrorTest('WrongModelName', 'ModelSyntaxTest3.dsl', ...
                                 'No model found with name: ModelSyntaxTest3'));
 
@@ -54,16 +64,20 @@ s_model.add(CreateUserErrorTest('WrongModelName', 'ModelSyntaxTest3.dsl', ...
 s_states = Suite('State Syntax Tests');
 s.add(s_states);
 
-s_states.add(CreateUserErrorTest('MispelledState', 'StateSyntaxTest1.dsl', ...
-                                 'Unknown identifier encountered: stat'))
-s_states.add(CreateUserErrorTest('StateNoVariable', 'StateSyntaxTest2.dsl', ...
-                                 'STATE keyword must be followed by'));
-s_states.add(CreateUserErrorTest('NoInitialValue', 'StateSyntaxTest3.dsl', ...
-                                 'Initial value for state x is not specified'));
+if mode == INTERNAL
+    s_states.add(CreateUserErrorTest('MispelledState', 'StateSyntaxTest1.dsl', ...
+                                     'Unknown identifier encountered: stat'))
+    s_states.add(CreateUserErrorTest('StateNoVariable', 'StateSyntaxTest2.dsl', ...
+                                     'STATE keyword must be followed by'));
+    s_states.add(CreateUserErrorTest('NoInitialValue', 'StateSyntaxTest3.dsl', ...
+                                     'Initial value for state x is not specified'));
+end
 s_states.add(CreateUserErrorTest('UndefinedProperty', 'StateSyntaxTest4.dsl', ...
                                  'Member SomeProperty not found in object .*x'));
-s_states.add(CreateUserErrorTest('NoWithForProperties', 'StateSyntaxTest5.dsl', ...
-                                 'Adding WITH keyword'));
+if mode == INTERNAL
+    s_states.add(CreateUserErrorTest('NoWithForProperties', 'StateSyntaxTest5.dsl', ...
+                                     'Adding WITH keyword'));
+end
 
 % Now create tests for equations
 s_equs = Suite('Equation Syntax Tests');
@@ -71,13 +85,15 @@ s.add(s_equs);
 
 s_equs.add(CreateUserErrorTest('MispelledEquation', ...
                                'EquationSyntaxTest1.dsl', 'Unknown identifier encountered: euation'));
-s_equs.add(CreateUserErrorTest('NoEquationKeyword', ...
-                               'EquationSyntaxTest2.dsl', 'Adding EQUATION keyword'));
-s_equs.add(CreateUserErrorTest('EQUATIONSnotEQUATION', ...
-                               'EquationSyntaxTest3.dsl', 'Removing EQUATIONS keyword'))
-s_equs.add(CreateUserErrorTest('NoRHS', ...
-                               'EquationSyntaxTest4.dsl', ['EQUATION '...
-                               'keyword must be followed by an equation']));
+if mode == INTERNAL
+    s_equs.add(CreateUserErrorTest('NoEquationKeyword', ...
+                                   'EquationSyntaxTest2.dsl', 'Adding EQUATION keyword'));
+    s_equs.add(CreateUserErrorTest('EQUATIONSnotEQUATION', ...
+                                   'EquationSyntaxTest3.dsl', 'Removing EQUATIONS keyword'))
+    s_equs.add(CreateUserErrorTest('NoRHS', ...
+                                   'EquationSyntaxTest4.dsl', ['EQUATION '...
+                                   'keyword must be followed by an equation']));
+end  
 
 
 % Tests for submodels
@@ -107,14 +123,16 @@ s.add(s_iters);
 s_iters.add(CreateUserErrorTest('DiscreteIteratorWithSolver', ...
                                 'IteratorSyntaxTest1.dsl', ...
                                 'Discrete time iterators can not have an integration method \(solver\) defined'))
-s_iters.add(CreateUserErrorTest('DiscreteIteratorWithDiffEq', ...
-                                'IteratorSyntaxTest2.dsl', ...
-                                'Unexpected derivative found with discrete iterator'))
-
+if mode == INTERNAL
+    s_iters.add(CreateUserErrorTest('DiscreteIteratorWithDiffEq', ...
+                                    'IteratorSyntaxTest2.dsl', ...
+                                    'Unexpected derivative found with discrete iterator'))
+end
 end
 
 % Iterator specific error tests
-function s = IteratorTests
+function s = IteratorTests(mode)
+INTERNAL = 0; RELEASE = 1;
 
 s = Suite('Iterator Tests');
 
@@ -127,18 +145,21 @@ s.add(CreateUserErrorTest('WrongIteratorIndex', 'IteratorTest3.dsl', ...
                           'can not use iterator']));
 s.add(CreateUserErrorTest('ForwardIndexing', 'IteratorTest4.dsl', ...
                           ['Invalid positive temporal index found on quantity']));
-s.add(CreateUserErrorTest('CurrentIndexingOnDiscreteState', 'IteratorTest5.dsl', ...
-                          ['Invalid temporal index on discrete state x. Discrete states must be defined as x.n.1. on the left hand side of equation']));
 s.add(CreateUserErrorTest('ForwardIndexingOnDiscreteState', 'IteratorTest6.dsl', ...
                           ['Invalid temporal index on discrete state x. Discrete states must be defined as x.n.1. on the left hand side of equation']));
-s.add(CreateUserErrorTest('InvalidIndexExpression', 'IteratorTest7.dsl', ...
-                          ['Invalid index detected on index of x']));
-s.add(CreateUserErrorTest('InvalidIteratorOnState', 'IteratorTest8.dsl', ...
-                          ['Temporal iterators can not be used as part of a state declaration']));
-
+if mode == INTERNAL
+    s.add(CreateUserErrorTest('CurrentIndexingOnDiscreteState', 'IteratorTest5.dsl', ...
+                              ['Invalid temporal index on discrete state x. Discrete states must be defined as x.n.1. on the left hand side of equation']));
+    s.add(CreateUserErrorTest('InvalidIndexExpression', 'IteratorTest7.dsl', ...
+                              ['Invalid index detected on index of x']));
+    s.add(CreateUserErrorTest('InvalidIteratorOnState', 'IteratorTest8.dsl', ...
+                              ['Temporal iterators can not be used as part of a state declaration']));
+end
+                      
 end
 
-function s = VariableTests
+function s = VariableTests(mode)
+INTERNAL = 0; RELEASE = 1;
 
 % Define a suite to check for undefined or over defined variables
 s = Suite('Variable Tests');
@@ -146,12 +167,16 @@ s = Suite('Variable Tests');
 % Start adding tests
 s.add(CreateUserErrorTest('UndefinedIntermediate', 'VariableTest1.dsl', ...
                           'Unknown identifier encountered: k'));
-s.add(CreateUserErrorTest('UndefinedDiffEquVariable', 'VariableTest2.dsl', ...
-                          'Derivative of quantity y not declared as a state is not supported'));
+if mode == INTERNAL
+    s.add(CreateUserErrorTest('UndefinedDiffEquVariable', 'VariableTest2.dsl', ...
+                              'Derivative of quantity y not declared as a state is not supported'));
+end
 s.add(CreateUserErrorTest('UndefinedOutputInHeader', 'VariableTest3.dsl', ...
                           'No quantity found matching output in header: y'));
-s.add(CreateUserErrorTest('NoOutputsInHeader', 'VariableTest4.dsl', ...
-                          'Model VariableTest4 does not have any outputs defined'));
+if mode == INTERNAL
+    s.add(CreateUserErrorTest('NoOutputsInHeader', 'VariableTest4.dsl', ...
+                              'Model VariableTest4 does not have any outputs defined'));
+end
 s.add(CreateUserErrorTest('DefinedOutputNotInHeader', 'VariableTest5.dsl', ...
                           'Output x does not appear as a returned quantity in the model header'));
 s.add(CreateUserErrorTest('InputAndEquation', 'VariableTest6.dsl', ...
@@ -171,7 +196,8 @@ end
 
 % Output Tests - find errors in outputs in single models and sub
 % models
-function s = OutputTests
+function s = OutputTests(mode)
+INTERNAL = 0; RELEASE = 1;
 
 s = Suite('Output Tests');
 
@@ -181,23 +207,27 @@ s.add(CreateUserErrorTest('OutputContentWithoutEquation', 'OutputTest2.dsl', ...
                           'Unknown identifier encountered: x'));
 s.add(CreateUserErrorTest('OutputConditionWithoutEquation', 'OutputTest3.dsl', ...
                           'Unknown identifier encountered: x'));
-s.add(CreateUserErrorTest('OutputContentsThroughSubmodels', 'OutputTest4.dsl', ...
-                          'Output .* in model .* can not be a grouping'));
-s.add(CreateUserWarningTest('OutputContentsThroughSubmodels', 'OutputTest5.dsl', ...
-                            'The condition .* for output .* in model .* is being ignored'));
-
+if mode == INTERNAL
+   s.add(CreateUserErrorTest('OutputContentsThroughSubmodels', 'OutputTest4.dsl', ...
+                             'Output .* in model .* can not be a grouping'));
+   s.add(CreateUserWarningTest('OutputContentsThroughSubmodels', 'OutputTest5.dsl', ...
+                               'The condition .* for output .* in model .* is being ignored'));
+end
 
 end
 
 % Equation Tests - find errors in equations
-function s = EquationTests
+function s = EquationTests(mode)
+INTERNAL = 0; RELEASE = 1;
 
 s = Suite('Equation Tests');
 
 s.add(CreateUserErrorTest('UndefinedFunction', 'EquationTest1.dsl', ...
                           'Unknown identifier encountered: f'))
-s.add(CreateUserErrorTest('DoubleAssignment', 'EquationTest2.dsl', ...
-                          'Use an EQUATIONS block to define more than one equation'))
+if mode == INTERNAL
+    s.add(CreateUserErrorTest('DoubleAssignment', 'EquationTest2.dsl', ...
+                              'Use an EQUATIONS block to define more than one equation'))
+end
 s.add(CreateUserErrorTest('DifferentialEquation', ...
                           'EquationTest3.dsl', 'Invalid derivative reference on symbol'))
 
