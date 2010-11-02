a64c445ed94ba5efad41f15dc6136ab6799c40e1
rweinstein <rweinstein@53642c02-b353-73e9-57f2-aeda9b58858d>
2010-11-02 18:17:26 +0000

    Fixed an issue where simex producing errors caused publishing to crash, causing the builds not to occur as expected...

--- SIMENGINE/trunk/interfaces/matlab/simex/simError.m
+++ SIMENGINE/trunk/interfaces/matlab/simex/simError.m
@@ -3,7 +3,18 @@ function [e] = simError(product, id, msg)
     
 % exn = MException(['Simatra:' product ':' id], msg);
 % exn.throw
-    stack = struct('file', which('simex'), 'name', 'simex', 'line', 1);
+    st = dbstack;
+    stack = st;
+    for i=1:length(st)
+        % seek out when we first find simex, then we can use that to see
+        % the remaining stack trace
+        if strcmp(st(i).name, 'simex')
+            stack = st(i:end);
+            break;
+        end
+    end
+    
+    %stack = struct('file', which('simex'), 'name', 'simex', 'line', 1);
     e = struct('identifier', ['Simatra:' product ':' id], 'message', msg, ...
                'stack', stack);
     error(e);
